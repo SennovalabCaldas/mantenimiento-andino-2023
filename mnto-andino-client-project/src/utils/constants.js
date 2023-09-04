d48c3f5b5fd26b30794e6d2316ffac042ea2b44d
SennovalabCaldas <sennovalabsennova@gmail.com>
2023-09-04 02:08:02 -0500

    Modify port

--- mnto-andino-client-project/src/utils/constants.js
+++ mnto-andino-client-project/src/utils/constants.js
@@ -2,8 +2,8 @@ const SERVER_IP = "0.0.0.0:27017";
 const API_VERSION = "v1";
 
 export const ENV = {
-  BASE_PATH: `http://${SERVER_IP}`,
-  BASE_API: `http://${SERVER_IP}/api/${API_VERSION}`,
+  BASE_PATH: `https://${SERVER_IP}`,
+  BASE_API: `https://${SERVER_IP}/api/${API_VERSION}`,
   API_ROUTES: {
     REGISTER: "auth/register",
     LOGIN: "auth/login",
