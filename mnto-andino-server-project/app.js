ec32b07b4dd136cdefc3a0b59944d76ff44202c8
SennovalabCaldas <sennovalabsennova@gmail.com>
2023-09-04 03:48:12 -0500

    Read the key and crt files

--- mnto-andino-server-project/app.js
+++ mnto-andino-server-project/app.js
@@ -50,8 +50,9 @@ app.use(`/api/${API_VERSION}/admin/clients`, clientRoutes);
 // Obtener la ruta absoluta de la carpeta actual
 const currentFolder = __dirname;
 // Validar existencia de archivos key y cert
-const keyPath = `${currentFolder}/../../../home/mntoandino/ssl/keys/99ac9_7e515_50f723af66f148b2e2702d04606367b8.key`;
-const certPath = `${currentFolder}/../../../home/mntoandino/ssl/certs/mantenimientoandino_co_99ac9_7e515_1725333050_fce0bdb052c6f002fe715187c3422759.crt`;
+const keyPath = "../../mntoandino/ssl/keys/99ac9_7e515_50f723af66f148b2e2702d04606367b8.key";
+const certPath = "../../mntoandino/ssl/certs/mantenimientoandino_co_99ac9_7e515_1725333050_fce0bdb052c6f002fe715187c3422759.crt";
+
 if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
   console.log("Archivos de clave y certificado encontrados.");
   // Crear servidor HTTPS solo si los archivos existen
