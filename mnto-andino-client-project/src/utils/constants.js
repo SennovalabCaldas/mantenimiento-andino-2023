8fb1a6c18e9c20bf34350a0a13365429d9b658e5
slesmes <sennovalabsennova@gmail.com>
2023-09-06 18:05:14 -0500

    Corrección ruta getMe

--- mnto-andino-client-project/src/utils/constants.js
+++ mnto-andino-client-project/src/utils/constants.js
@@ -5,15 +5,17 @@ export const ENV = {
   BASE_PATH: SERVER_IP,
   BASE_API: `${SERVER_IP}/api/${API_VERSION}`,
   API_ROUTES: {
+    USER_ME: "user/me",
+    UPDATE_ME: "user/edit-profile",
+    /* users */
+    USER: "user",
+    GET_USER: "get-user",
+
     REGISTER: "auth/register",
     LOGIN: "auth/login",
     RESET_PASSWORD: "auth/change-password",
     PASSWORD_RECOVERY: "auth/password-recovery",
     REFRESH_TOKEN: "auth/refresh-access-token",
-    USER_ME: "admin/user/me",
-    UPDATE_ME: "user/edit-profile",
-    USER: "user",
-    GET_USER: "get-user",
     USERS: "admin/users",
     SEDE: "sedes/sede",
     SEARCH_SEDES: "sedes/buscar",
