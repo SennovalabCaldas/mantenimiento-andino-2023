9a046a084fe5ebb591d4fe6c6f2cf40b4eac4216
slesmes <sennovalabsennova@gmail.com>
2023-09-06 12:28:11 -0500

    pri

--- mnto-andino-server-project/index.js
+++ mnto-andino-server-project/index.js
@@ -1,3 +1,33 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const app = express();
+// const PORT = process.env.PUERTO || 3000; // Cambia 3000 a 443 para HTTPS
+
+// // Middleware para verificar si la solicitud es HTTP o HTTPS
+// app.use((req, res, next) => {
+//   if (req.secure) {
+//     next();
+//   } else {
+//     res.redirect(`https://72.167.135.41`);
+//   }
+// });
+
+// console.log(`mongodb://prueba2:prueba2@0.0.0.0:27017/mnto-andino-db`);
+// mongoose
+//   .connect(`mongodb://prueba2:prueba2@0.0.0.0:27017/mnto-andino-db`, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Conexión a la base de datos exitosa");
+
+//     app.listen(PORT, () => {
+//       console.log(`Servidor Express en funcionamiento en el puerto 3000`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error conectando a la base de datos:", error);
+//   });
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -8,33 +38,24 @@ app.use((req, res, next) => {
   if (req.secure) {
     next();
   } else {
-    res.redirect(`https://${req.hostname}${req.url}`);
+    res.redirect(`https://72.167.135.41`);
   }
 });
 
-app.use((req, res, next) => {
-  console.log("Hostname:", req.hostname);
-  console.log("URL:", req.url);
-  next(); // Llama a next() para continuar con el flujo de la solicitud
-});
-
-app.get("/holii", (req, res) => {
-  res.send("Hola, mundo!");
-});
-
-console.log(`mongodb://prueba2:prueba2@0.0.0.0:27017/mnto-andino-db`);
+// Conexión a la base de datos remota
+const uri = "mongodb://prueba2:prueba2@72.167.135.41:27017/mnto-andino-db";
 mongoose
-  .connect(`mongodb://prueba2:prueba2@0.0.0.0:27017/mnto-andino-db`, {
+  .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Conexión a la base de datos exitosa");
+    console.log("Conexión a la base de datos remota exitosa");
 
     app.listen(PORT, () => {
       console.log(`Servidor Express en funcionamiento en el puerto 3000`);
     });
   })
   .catch((error) => {
-    console.error("Error conectando a la base de datos:", error);
+    console.error("Error conectando a la base de datos remota:", error);
   });
