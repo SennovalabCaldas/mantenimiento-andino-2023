c25049c425c6e5fbf19dd52db16c49759ebedc17
slesmes <sennovalabsennova@gmail.com>
2023-09-06 19:42:58 -0500

    Verify port

--- mnto-andino-server-project/index.js
+++ mnto-andino-server-project/index.js
@@ -2,16 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { app, printRoutes } = require("./app");  // Importa la configuración de Express desde app.js
-const PORT = process.env.PUERTO || 3000; // Cambia 3000 a 443 para HTTPS
-
-// Middleware para verificar si la solicitud es HTTP o HTTPS
-// app.use((req, res, next) => {
-//   if (req.secure) {
-//     next();
-//   } else {
-//     res.redirect(`https://72.167.135.41`);
-//   }
-// });
+const PORT = process.env.PUERTO || 3000; 
 
 // Conexión a la base de datos remota
 const uri = "mongodb://prueba2:prueba2@72.167.135.41:27017/mnto-andino-db";
