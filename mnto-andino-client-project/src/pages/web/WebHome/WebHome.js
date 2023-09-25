dd655a5b1c625ed9b978685f63d0a79743e6f593
YanethM <sennovalabsennova@gmail.com>
2023-09-24 19:02:19 -0500

    Add funcionalities and modules

--- mnto-andino-client-project/src/pages/web/WebHome/WebHome.js
+++ mnto-andino-client-project/src/pages/web/WebHome/WebHome.js
@@ -1,63 +1,83 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./WebHome.scss";
 import { useSelector } from "react-redux";
-export const WebHome = () => {
-  const news = useSelector((state) => state.post.allPosts);
-  console.log(news);
-  useEffect(() => {
-    [].slice
-      .call(document.querySelectorAll('a[href="#"'))
-      .forEach(function (el) {
-        el.addEventListener("click", function (ev) {
-          ev.preventDefault();
-        });
-      });
-  }, []);
+
+const Slide = ({ post, isActive }) => {
   return (
-    <div>
-      {news.map((post) => (
-        <div className="demo-cont">
-          <div className="fnc-slider example-slider">
-            <div className="fnc-slider__slides">
-              <div className="fnc-slide m--blend-green m--active-slide">
-                <div className="fnc-slide__inner">
-                  <div className="fnc-slide__mask">
-                    <div className="fnc-slide__mask-inner"></div>
-                  </div>
-                  <div className="fnc-slide__content">
-                    <h2 className="fnc-slide__heading">
-                      <div className="fnc-slide__heading-line">
-                        <span> {post.titulo}</span>
-                      </div>
-                      <div className="fnc-slide__heading-line">
-                        <span> {post.subtitulo}</span>
-                      </div>
-                    </h2>
-                    <button type="button" className="fnc-slide__action-btn">
-                      Credits
-                      <span data-text="Credits">Credits</span>
-                    </button>
-                  </div>
-                </div>
+    <div className={`fnc-slider__slides ${isActive ? "m--active-slide" : ""}`}>
+      <div className="fnc-slide m--blend-green">
+        <div className="fnc-slide__inner">
+          <div className="fnc-slide__mask">
+            <div className="fnc-slide__mask-inner"></div>
+          </div>
+          <div className="fnc-slide__content">
+            <h2 className="fnc-slide__heading">
+              <div className="fnc-slide__heading-line">
+                <span> {post.titulo}</span>
               </div>
-            </div>
-            <nav className="fnc-nav">
-              <div className="fnc-nav__bgs">
-                <div className="fnc-nav__bg m--navbg-green m--active-nav-bg"></div>
-                <div className="fnc-nav__bg m--navbg-dark"></div>
-                <div className="fnc-nav__bg m--navbg-red"></div>
-                <div className="fnc-nav__bg m--navbg-blue"></div>
+              <div className="fnc-slide__heading-line">
+                <span> {post.subtitulo}</span>
               </div>
-              <div className="fnc-nav__controls">
-                <button className="fnc-nav__control">
-                  {post.titulo}
-                  <span className="fnc-nav__control-progress"></span>
-                </button>
-              </div>
-            </nav>
+            </h2>
+            <button type="button" className="fnc-slide__action-btn">
+              Ver más
+              <span data-text="Credits">Ver más</span>
+            </button>
           </div>
         </div>
-      ))}
+      </div>
+    </div>
+  );
+};
+
+export const WebHome = () => {
+  const news = useSelector((state) => state.post.allPosts);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [isAutoPlay, setIsAutoPlay] = useState(true);
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    if (isAutoPlay) {
+      timeoutRef.current = setTimeout(() => {
+        nextSlide();
+      }, 4000); 
+    }
+
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, [currentSlide, isAutoPlay]);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % news.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? news.length - 1 : prevSlide - 1
+    );
+  };
+
+  const toggleAutoPlay = () => {
+    setIsAutoPlay(!isAutoPlay);
+  };
+
+  return (
+    <div>
+      <div className="demo-cont">
+        <div className="fnc-slider example-slider">
+          {news.map((post, index) => (
+            <Slide key={index} post={post} isActive={index === currentSlide} />
+          ))}
+        </div>
+      </div>
+      <div className="slider-controls">
+        <button onClick={prevSlide}>Anterior</button>
+        <button onClick={nextSlide}>Siguiente</button>
+        <button onClick={toggleAutoPlay}>
+          {isAutoPlay ? "Pausar" : "Reproducir"}
+        </button>
+      </div>
     </div>
   );
 };
