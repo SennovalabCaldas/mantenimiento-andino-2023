25b86ce5bbacf7956194d904f6b1eea827224bae
SennovalabCaldas <sennovalabsennova@gmail.com>
2023-11-26 16:13:00 -0500

    CHenges in server

--- mnto-andino-server-project/app.js
+++ mnto-andino-server-project/app.js
@@ -35,7 +35,7 @@ app.use("/uploads", express.static("uploads"));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization, Accept" );
   next();
 });
 
